e3d930b03fe7c1d9d9b148fdb12e9ba84e388608
Miroslav Mikus <miroslav.mikus@outlook.com>
2021-03-15 09:08:29 +0100

    Add SimpleExec

---
+++ Exercise.SimpleExec/Exercise.Simple_Exec/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleExec;
+
+namespace Exercise.Simple_Exec
+{
+    class Program
+    {
+        //static async Task Main(string[] args)
+        //{
+        //     await Command.RunAsync("notepad");
+        //}
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine(await GitStatus());
+            Console.ReadLine();
+        }
+
+        static Task<string> GitStatus()
+        {
+            return Command.ReadAsync("git", "status", Environment.CurrentDirectory);
+        }
+    }
+}
