7136b7eb320b184bbe6831b556311e1aaa8918a2
Miroslav Mikus <miroslav.mikus@outlook.com>
2021-03-15 09:06:45 +0100

    Add Quartz

---
+++ Exercise.Quartz/Exercise.Quartz/Jobs/ConsoleJob.cs
@@ -0,0 +1,26 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Quartz.Jobs
+{
+    class ConsoleJob : IJob
+    {
+        public string Name { get; }
+
+        public ConsoleJob(string name)
+        {
+            Name = name;
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine($"Name: {Name} - Hi, Im console Job");
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Exercise.Quartz/Exercise.Quartz/Program.cs
@@ -0,0 +1,75 @@
+﻿using Exercise.Quartz.Jobs;
+using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Exercise.Quartz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JobWithMapData();
+
+            Console.WriteLine("Done");
+
+            Console.ReadLine();
+        }
+
+        public static void JobWithMapData()
+        {
+            ISchedulerFactory factory = new StdSchedulerFactory();
+
+            IScheduler scheduler = factory.GetScheduler().Result;
+
+            IDictionary<string, object> values = new Dictionary<string, object>() { { "Name", "I'm Miro" } };
+
+            IJobDetail job = JobBuilder.Create<ConsoleJob>()
+                                .WithIdentity("jobName", "JobGroup")
+                                .UsingJobData(new JobDataMap(values))
+                                .Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                                .WithSimpleSchedule(a => a.WithIntervalInSeconds(2).WithRepeatCount(5))
+                                .StartNow()
+                                .Build();
+
+            scheduler.ScheduleJob(job, trigger);
+
+            scheduler.Start();
+
+            Thread.Sleep(TimeSpan.FromMinutes(5));
+
+        }
+
+        public static void SimpleJob()
+        {
+            ISchedulerFactory factory = new StdSchedulerFactory();
+
+            IScheduler scheduler = factory.GetScheduler().Result;
+
+            IJobDetail job = JobBuilder.Create<ConsoleJob>()
+                                .WithIdentity("jobName", "JobGroup")
+                                .UsingJobData("Name", "JobDataName")
+                                .Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                                .WithSimpleSchedule(a => a.WithIntervalInSeconds(2).WithRepeatCount(5))
+                                .StartNow()
+                                .Build();
+
+            scheduler.ScheduleJob(job, trigger);
+
+            scheduler.Start();
+
+            Thread.Sleep(TimeSpan.FromMinutes(1));
+
+            scheduler.Shutdown();
+        }
+    }
+}
