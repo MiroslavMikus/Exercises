5dc4b3d9e3e2e8c9e9de3ff3a4367c4983dddf3f
Miroslav Mikus <miroslav.mikus@outlook.com>
2021-03-30 19:54:27 +0200

    MSConfig add test project

---
+++ cs.MicrosoftConfiguration/HalloConfig/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+
+namespace HalloConfig
+{
+    class Program
+    {
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile(
+                $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json",
+                optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
