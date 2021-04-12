2baa36f0b3c79dd12299fcf86aea6221e191f04b
Miroslav Mikus <miroslav.mikus@outlook.com>
2021-04-12 14:35:06 +0200

    WSDL Init project

---
+++ cs.WSDL/Hallo.WSDL/Menu/SpectreMenuPrinter.cs
@@ -0,0 +1,37 @@
+﻿using Spectre.Console;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Hallo.WSDL.Menu
+{
+    public class SpectreMenuPrinter
+    {
+        public static void Exit() => Environment.Exit(0);
+
+        public async Task Print()
+        {
+            while (true)
+            {
+                var command = AnsiConsole.Prompt(new SelectionPrompt<MethodInfo>().Title("[green]Select command[/]")
+                    .AddChoices(ReflectionTools.GetCommands(typeof(Program)).OrderBy(a => a.Name))
+                    .AddChoice(typeof(SpectreMenuPrinter).GetMethods().Single(a => a.Name == "Exit"))
+                    .UseConverter(a => a.Name.Replace("Command", "")));
+
+                Console.Clear();
+
+                var result = command.Invoke(null, null);
+
+                if (result is Task resultTask)
+                {
+                    await resultTask;
+                }
+
+                AnsiConsole.Markup("[underline red]Return to menu[/]");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ cs.WSDL/Hallo.WSDL/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Hallo.WSDL.Menu;
+using Hallo.WSDL.TestService;
+using Spectre.Console;
+
+namespace Hallo.WSDL
+{
+    class Program
+    {
+        private static TempConvertSoapClient _client;
+
+        static async Task Main(string[] args)
+        {
+            _client = new TestService.TempConvertSoapClient();
+            
+            var menu = new SpectreMenuPrinter();
+            await menu.Print();
+        }
+
+        public static void FahrenheitToCelsius()
+        {
+            var fahrenheit = AnsiConsole.Ask<int>("Enter [green]fahrenheit[/] value:");
+
+            var result = _client.FahrenheitToCelsius(fahrenheit.ToString());
+            
+            AnsiConsole.WriteLine("Your result is [green]{0}°C", result);
+
+            Console.ReadLine();
+        }
+    }
+}
