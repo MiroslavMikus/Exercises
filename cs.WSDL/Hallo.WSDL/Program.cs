d10a54cdad0c708428c0f9295f95e0040b307693
Miroslav Mikus <miroslav.mikus@outlook.com>
2021-04-12 16:51:39 +0200

    WSDL add test service

--- cs.WSDL/Hallo.WSDL/Program.cs
+++ cs.WSDL/Hallo.WSDL/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ServiceModel;
 using System.Threading.Tasks;
 using Hallo.WSDL.Menu;
 using Hallo.WSDL.TestService;
@@ -6,24 +7,33 @@ using Spectre.Console;
 
 namespace Hallo.WSDL
 {
-    class Program
+    public static class Program
     {
         private static TempConvertSoapClient _client;
 
         static async Task Main(string[] args)
         {
-            _client = new TestService.TempConvertSoapClient();
+            var binding = new BasicHttpBinding
+            {
+                MaxReceivedMessageSize = 1_000_000,
+            };
+            
+            var endpoint = new EndpointAddress("http://www.w3schools.com/xml/tempconvert.asmx");
+
+            // var se = new ServiceEndpoint(new ContractDescription("TempConvertSoap"));
+
+            _client = new TestService.TempConvertSoapClient(binding, endpoint);
             
             var menu = new SpectreMenuPrinter();
             await menu.Print();
         }
 
-        public static void FahrenheitToCelsius()
+        public static void FahrenheitToCelsiusCommand()
         {
             var fahrenheit = AnsiConsole.Ask<int>("Enter [green]fahrenheit[/] value:");
 
             var result = _client.FahrenheitToCelsius(fahrenheit.ToString());
-            
+
             AnsiConsole.WriteLine("Your result is [green]{0}°C", result);
 
             Console.ReadLine();
